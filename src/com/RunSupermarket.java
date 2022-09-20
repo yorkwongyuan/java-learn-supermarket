e43294328759199a0da5ff2ed39a2bd8bdc32c56
yuan_wy_wang <631824375@qq.com>
2022-09-20 23:08:57 +0800

    fix: 完善购物逻辑

--- src/com/RunSupermarket.java
+++ src/com/RunSupermarket.java
@@ -3,6 +3,8 @@ import com.person.Customer;
 import com.supermarket.LittleSuperMarket;
 import com.supermarket.Products;
 
+import java.util.Scanner;
+
 public class RunSupermarket {
     public static void main(String[] args) {
         LittleSuperMarket littleSuperMarket = new LittleSuperMarket();
@@ -26,6 +28,7 @@ public class RunSupermarket {
         System.out.println("超市开张了");
 
         boolean open = true;
+        Scanner scanner = new Scanner(System.in);
         while (open) {
             System.out.println("本店名字叫做" + littleSuperMarket.name);
             System.out.println("本店地址为" + littleSuperMarket.address);
@@ -54,6 +57,40 @@ public class RunSupermarket {
             } else {
                 System.out.println("民众皆成幻影" + customer.name);
             }
+
+            double totalCost = 0;
+//            购物逻辑
+            while (true) {
+                System.out.println("本店提供" + all.length + "种商品，欢迎选购。请输入商品编号");
+                int index = scanner.nextInt();
+                if (index < 0) {
+                    break;
+                }
+                Products m = all[index];
+                System.out.println("你选购的商品名称是" + m.name + ".单价是" + m.soldPrice + ".请问你要买多少个");
+                int numToBuy = scanner.nextInt();
+
+                totalCost += numToBuy * m.soldPrice;
+//              商品库存减少
+                m.count -= numToBuy;
+//                销售数量增加
+                littleSuperMarket.productSold[index] += numToBuy;
+            }
+//    购物完成后
+            customer.money -= totalCost;
+
+//            开车来的，离开后车位+1
+            if (customer.isDriving) {
+                littleSuperMarket.packingCount++;
+            }
+
+            System.out.println("顾客" + customer.name + "共消费" + totalCost);
+//            超市收入
+            littleSuperMarket.incomeSum = totalCost;
+            System.out.println("还继续营业吗？");
+            open = scanner.nextBoolean();
         }
+        System.out.println("超市关门了");
+        System.out.println("今天的营业额为" + littleSuperMarket.incomeSum + ".");
     }
 }
